5622bffc56cc5f93a14b533b4c494856eb655829
lij008 <34274390+lij008@users.noreply.github.com>
2024-02-27 00:17:18 +0000

    Add files via upload

--- provision.sh
+++ provision.sh
@@ -11,12 +11,12 @@ sudo rm -rf /var/www/html/*
 sudo git clone https://github.com/lij008/hackthisbox.git /var/www/html/
 
 # Install Apache.
-# sudo apt install -y apache2
-# sudo service apache2 start
+sudo apt install -y apache2
+sudo service apache2 start
 
 # Install MariaDB.
-# sudo apt install -y mariadb-server
-# sudo service mariadb start
+sudo apt install -y mariadb-server
+sudo service mariadb start
 
 # Install PHP.
 sudo apt install -y php
@@ -24,11 +24,11 @@ sudo apt install -y php-mysqli
 sudo service apache2 restart
 
 # Set up database.
-# sudo mysql -uroot < /var/www/sql/db.sql
+sudo mysql -uroot < /var/www/sql/db.sql
 
 # Secure MariaDB installation.
-# sudo apt install -y expect
-# sudo expect /var/www/resources/db_secure.exp $DB_PASS
+sudo apt install -y expect
+sudo expect /var/www/resources/db_secure.exp $DB_PASS
 
 # Set database password for website.
 sudo sed -i -e "s/define('DB_PASS', '');/define('DB_PASS', '$DB_PASS');/g" /var/www/web/db_configuration.php
@@ -37,7 +37,7 @@ sudo sed -i -e "s/define('DB_PASS', '');/define('DB_PASS', '$DB_PASS');/g" /var/
 cd /var/www/html
 
 # Create symlink into project web root.
-sudo ln -s web html
+# sudo ln -s web html
 
 # Install NPM.
 # yum install -y epel-release
